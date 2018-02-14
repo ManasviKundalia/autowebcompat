c48b9d2693ea6e0ff175792837a111a13d8af701
Manasvi Kundalia <kmanasvi.paresh.eee13@iitbhu.ac.in>
2018-02-14 07:29:07 +0000

    Update get_dependencies.py
    fixing filename finder in get_dependencies.py

--- get_dependencies.py
+++ get_dependencies.py
@@ -3,9 +3,18 @@ import os
 import tarfile
 from zipfile import ZipFile
 import requests
+from rfc6266 import parse_headers
 
 
-def download(url, filename):
+def download(url):
+    response = requests.get(url)
+    cd = response.headers['content-disposition']
+    filename = parse_headers(cd).filename_unsafe
+    
+    if not filename:
+        print('Couldn\'t get filename for this url')
+        return 
+    
     with open(filename, 'wb') as f:
         response = requests.get(url, stream=True)
         total = response.headers.get('content-length')
@@ -22,28 +31,29 @@ def download(url, filename):
                 sys.stdout.write('\r[{}{}]'.format('█' * done, '.' * (50 - done)))
                 sys.stdout.flush()
     sys.stdout.write('\n')
+    return filename
 
 
 if sys.platform.startswith('linux'):
     url = 'https://www.dropbox.com/s/ziti4nkdzhgwg1n/linux.tar.xz?dl=1'
-    name = 'linux.tar.xz'
 elif sys.platform.startswith('darwin'):
     url = 'https://www.dropbox.com/s/k4yifantsypy9xv/mac.tar.xz?dl=1'
-    name = 'mac.tar.xz'
 elif sys.platform.startswith('win32'):
     url = 'https://www.dropbox.com/s/xskj9rpn2fjkra8/win32.tar.xz?dl=1'
-    name = 'win32.tar.xz'
 
 print('[*] Downloading support files...')
-download(url, name)
-
-print('[*] Extracting files...')
-with tarfile.open(name, 'r:xz') as f:
-    f.extractall('.')
-os.remove(name)
-
+#download(url, name)
+name = download(url)
+try:
+    print('[*] Extracting files...')
+    with tarfile.open(name, 'r:xz') as f:
+        f.extractall('.')
+    os.remove(name)
+except:
+    print('bad url')
+    
 print('[*] Downloading data.zip...')
-download('https://www.dropbox.com/s/7f5uok2alxz9j1r/data.zip?dl=1', 'data.zip')
+download('https://www.dropbox.com/s/7f5uok2alxz9j1r/data.zip?dl=1')
 
 print('[*] Extracting data.zip...')
 with ZipFile('data.zip', 'r') as z:
